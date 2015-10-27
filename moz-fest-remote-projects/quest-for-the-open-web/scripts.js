4ce70338626fc22db8bb050258ca95da522fe8c1
chadsansing <csansing@gmail.com>
2015-10-27 12:49:27 -0400

    Updated math on quest-for-the-open-web scripts

--- moz-fest-remote-projects/quest-for-the-open-web/scripts.js
+++ moz-fest-remote-projects/quest-for-the-open-web/scripts.js
@@ -5,9 +5,9 @@ function fantasyName() {
 
 var adjectives = ['bold', 'brave', 'daring', 'ardent', 'true', 'ultimate', 'heroic', 'invincible', 'honest', 'industrious', 'wily', 'uncanny', 'crafty', 'genius', 'courageous', 'indomitable', 'indefatigable', 'indestructible', 'unstoppable', 'worthy', 'inspiring', 'quick', 'speedy', 'kind', 'wise', 'sage', 'compassionate', 'rebellious', 'revolutionary', 'smiling', 'feisty', 'tenacious', 'unyielding', 'omnipresent', 'omniscient', 'all-knowing', 'telephonic', 'holographic', 'empathic', 'sensational', 'gutsy'];
 
-var classes = ['coder', 'teacher', 'captain', 'leader', 'programmer', 'artist', 'painter', 'dreamer', 'writer', 'explorer', 'gamer', 'organizer', 'activist', 'liberator', 'designer', 'maker', 'builder', 'guide', 'mentor', 'educator', 'librarian', 'crafter', 'tinkerer', 'sculptor', 'composer', 'dancer', 'elder', 'counsellor', 'puppeteer', 'director', 'associate', 'lead', 'visionary'];
+var classes = ['coder', 'VP', 'fellow', 'researcher', 'scholar', 'scientist', 'reporter', 'teacher', 'captain', 'leader', 'programmer', 'artist', 'painter', 'dreamer', 'writer', 'explorer', 'gamer', 'organizer', 'activist', 'liberator', 'designer', 'maker', 'builder', 'guide', 'mentor', 'educator', 'librarian', 'crafter', 'tinkerer', 'sculptor', 'composer', 'dancer', 'elder', 'counsellor', 'puppeteer', 'director', 'associate', 'lead', 'visionary'];
 
-var origins = ['the Open Web', 'open educational resources', 'youth programming', 'museum education', 'Web-native apps', 'remix', 'mash-up', 'hackery', 'hack jams', 'game development', 'coding for the Web', 'the stack', 'snacks', 'coffee', 'scripting', 'Web styles', 'HTML', 'CSS', 'Web literacy', 'project management', 'debugging', 'localization', 'equity', 'equality', 'diversity', 'inclusion', 'collaboration', 'community', 'contribution ladders', 'the Internet', 'traceroute', 'privacy', 'security', 'encryption', 'inception', 'MozFest', 'Thimble', 'Popcorn', 'Appmaker', 'Firefox OS', 'mobile development', 'open science', 'open journalism', 'meta-wrangling', 'space-wrangling', 'Ravensbourne', 'London', 'open source', 'transparency', 'make it, eff it, ship it', 'dev sprints', 'caffeine', 'storytelling', 'user experience', 'graphic design', 'Turtle Wushu', 'karaoke', 'the commons', 'open data', 'the distro', 'the fork', 'pull requests', 'Open'];
+var origins = ['the Open Web', 'heartbeats', 'redpens', 'wireframes', 'open educational resources', 'youth programming', 'museum education', 'Web-native apps', 'remix', 'mash-up', 'hackery', 'hack jams', 'game development', 'coding for the Web', 'the stack', 'snacks', 'coffee', 'scripting', 'Web styles', 'HTML', 'CSS', 'Web literacy', 'project management', 'debugging', 'localization', 'equity', 'equality', 'diversity', 'inclusion', 'collaboration', 'community', 'contribution ladders', 'the Internet', 'traceroute', 'privacy', 'security', 'encryption', 'inception', 'MozFest', 'Thimble', 'reverse image search', 'clubs', 'Firefox OS', 'mobile development', 'open science', 'open journalism', 'meta-wrangling', 'space-wrangling', 'Ravensbourne', 'London', 'open source', 'transparency', 'make it, eff it, ship it', 'dev sprints', 'caffeine', 'storytelling', 'user experience', 'graphic design', 'Turtle Wushu', 'karaoke', 'the commons', 'open data', 'the distro', 'the fork', 'pull requests', 'Open'];
 
 var buffs= ["+1", "+2", "+3", "+5"];
 
@@ -17,11 +17,11 @@ var skills = ["memes", "design sketches", "mock-ups", "brainstorms", "kittehs", 
 //this.altName = altNames[Math.round(Math.random()*altNames.length)];
 
 this.name = document.getElementById('myName').value;
-this.adjective = adjectives[Math.round(Math.random()*adjectives.length)];
-this.class = classes[Math.round(Math.random()*classes.length)];
-this.origin = origins[Math.round(Math.random()*origins.length)];
-this.buff = buffs[Math.round(Math.random()*3)];
-this.skill = skills[Math.round(Math.random()*skills.length)];
+this.adjective = adjectives[Math.round(Math.random()*(adjectives.length-1))];
+this.class = classes[Math.round(Math.random()*(classes.length-1))];
+this.origin = origins[Math.round(Math.random()*(origins.length-1))];
+this.buff = buffs[Math.round(Math.random()*(buffs.length-1))];
+this.skill = skills[Math.round(Math.random()*(skills.length-1))];
 
 //alt names output
 //document.getElementById('myTrueName').innerHTML = "<div>I am "+ this.altName + ", the " + this.adjective + " " + this.class + " of " + this.origin + ".</div>"
