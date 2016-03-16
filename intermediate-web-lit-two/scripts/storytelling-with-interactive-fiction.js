c152713473bef0d3edeb47798dec9ab9fc7896ea
chadsansing <csansing@gmail.com>
2016-03-16 08:58:17 -0400

    Continued rough cut styling of intermediate-web-lit-two activities

--- intermediate-web-lit-two/scripts/storytelling-with-interactive-fiction.js
+++ intermediate-web-lit-two/scripts/storytelling-with-interactive-fiction.js
@@ -1,5 +1,3 @@
-//With help from 
-
 function twoDoors() {
 	
 	document.getElementById("story-space").innerHTML = "<div><p>Would you rather eat PANCAKES or WAFFLES?</p><pType your answer: <form><input type='text' name='box1' id='box1' oninput='foodRain()'></form></p></div>"
@@ -8,15 +6,66 @@ function twoDoors() {
 
 function foodRain() {
 	
-	 var myFav = document.getElementById('box1'); 
+	 var myFav = document.getElementById('box1');
 	 
-	 if(myFav.value == 'pancakes') {
-	  document.getElementById("story-space").innerHTML = "<div><p>How fortunate. Look outside - it's raining pancakes!</p></div>"
+	 if (myFav.value == 'pancakes') {
+	  document.getElementById('story-space').innerHTML = "<div><p>How fortunate. Look outside - it's raining pancakes!</p><p>SYRUP or ICE CREAM?</p><p><form><input name='box2' id='box2' type='text' oninput='toppings()'></form></p></div>"
+		 
+		 console.log("Pancake machine = science.");
 	 }
 	 else if (myFav.value == 'waffles') {
-	   document.getElementById("story-space").innerHTML = "<div><p>Oh, bummer. I only have a machine to make it rain pancakes.</p></div>"
+	   document.getElementById('story-space').innerHTML = "<div><p>Oh, bummer. I only have a machine to make it rain pancakes.</p><p>SYRUP or ICE CREAM?</p><p><form><input name='box2' id='box2' type='text' oninput='toppings()'></form></p></div>"
+		 
+		 console.log("Boo, waffles.");
 	 }
+	 
 	 else {
-		 console.log("Waiting for input.");
+		 console.log("Waiting....");
+	 }
+	 
+}
+
+function toppings() {
+
+	var myTopping = document.getElementById('box2');
+
+
+	if (myTopping.value == 'syrup') {
+   	 	document.getElementById('story-space').innerHTML = "<div><p>We are out of syrup. Here, take these pickles.</p><p>GAME OVER.</div>"
+ 
+		console.log("Don't try this at home.");
+	 }
+
+	 else if (myTopping.value == 'ice cream') {
+	 	document.getElementById('story-space').innerHTML = "<div><p>Is this dessert for breakfast or breakfast for dessert?</p><p><form><input name='box3' id='box3' type='text' oninput='newBrunch()'></form></p></div>"
+ 
+		 console.log('Winning.');
+
+	 }
+
+	 else {
+		 console.log("Waiting....");
+	 }
+ }
+ 
+function newBrunch() {
+	
+	var myNewWord = document.getElementById('box3');
+	
+	if (myNewWord.value == 'breakfast for dessert') {
+   	 	document.getElementById('story-space').innerHTML = "<div><p>Enjoy your bressert. It's the new dunch.</p><p>GAME OVER.</div>"
+ 
+		console.log("Bon appetit.");
+	 }
+
+	 else if (myNewWord.value == 'dessert for breakfast') {
+	 	document.getElementById('story-space').innerHTML = "<div><p>There's nothing better than a good dreakfast. It's the new linner.</p><p>GAME OVER.</p></div>"
+ 
+		 console.log('Enjoy.');
+
+	 }
+
+	 else {
+		 console.log("Waiting....");
 	 }
 }
