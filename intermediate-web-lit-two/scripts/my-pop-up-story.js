7b3082284e05aa248b7489944915a498093a9aad
chadsansing <csansing@gmail.com>
2016-03-17 07:42:01 -0400

    Edited session pages' layouts and drafted session01-buttons-and-alerts.html for intermediate-web-lit-two project

--- intermediate-web-lit-two/scripts/my-pop-up-story.js
+++ intermediate-web-lit-two/scripts/my-pop-up-story.js
@@ -1,3 +1,6 @@
 function popUp() {
-	alert("Once upon a time last Tuesday, a talking sandwich convinced me to eat soup instead.")
+	
+	var microStory = "Once upon a time last Tuesday, a talking sandwich convinced me to eat soup instead.";
+	
+	alert(microStory)
 }
